2e031bfee354005baaeb5dead324e7ab6fd7ad14
Tulasi Atikamsetti <tulasiatikamsetti@oiditss-MacBook-Pro.local>
2020-10-09 10:25:19 -0700

    CANVAS-138 - Populated the list of courses to be build.

---
+++ src/server/api/forms/courses.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const router = express.Router();
+const registrar = require('../../services/registrar');
+
+router.post('/', (req, res, err) => {
+  (async () => {
+    const { termCode, subjectAreaCode: department } = req.body;
+
+    const url = `/sis/api/v1/Classes/${termCode}`;
+    try {
+      const result = await registrar.call({
+        url,
+        params: {
+          offeredTermCode: termCode,
+          subjectAreaCode: department,
+          PageSize: 0,
+        },
+      });
+      // Console.log(result);
+      if (!result) {
+        throw new Error('No results');
+      }
+      const newResult = [];
+      result.classes.forEach((resObj) => {
+        const courseList = [];
+        const {
+          offeredTermCode,
+          courseCatalogNumberDisplay,
+          termSessionGroupCollection,
+          subjectAreaCode,
+        } = resObj;
+
+        termSessionGroupCollection.forEach((term) =>
+          term.classCollection.forEach((classObj) => {
+            courseList.push(
+              `${courseCatalogNumberDisplay}-${parseInt(classObj.classNumber)}`
+            );
+          })
+        );
+        newResult.push({ offeredTermCode, subjectAreaCode, courseList });
+      });
+
+      res.send(newResult);
+    } catch (errorMsg) {
+      err(errorMsg);
+    }
+  })();
+});
+module.exports = router;
--- src/server/api/forms/index.js
+++ src/server/api/forms/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const termsRoute = require('./terms');
 const sessionsRoute = require('./sessions');
 const subjareaRoute = require('./subjectareas');
+const courseRoute = require('./courses');
 
 router.use('/getTerms', termsRoute);
 router.use('/getSessions', sessionsRoute);
 router.use('/getSubjectareas', subjareaRoute);
+router.use('/getCourses', courseRoute);
 
 module.exports = router;
